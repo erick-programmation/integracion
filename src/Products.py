e22473a471e44a2c7b4edbea144b8c4ee4720f5d
MELASLAVO <er.munozc@duocuc.cl>
2024-05-14 22:28:05 -0400

    pruebas

--- src/Products.py
+++ src/Products.py
@@ -1,4 +1,55 @@
-products = [
-    {"name":"producto"},
-    {"name":"producto 2"}
-]
+from flask import Flask, jsonify, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configuración de la conexión a MySQL
+conn = mysql.connector.connect(
+    host="localhost",
+    user="Integracion2",
+    password="Integracion2",
+    database="Integracion2"
+)
+cursor = conn.cursor()
+
+# Ruta para obtener todos los datos de la tabla product con sus precios
+@app.route('/Products', methods=['GET'])
+def obtener_productos_con_precios():
+    cursor.execute("""
+        SELECT 
+            product.Código_del_producto AS Codigo_del_producto,
+            product.Marca,
+            product.Código AS Codigo,
+            product.Nombre,
+            precio.Fecha AS Fecha,
+            precio.valor AS Valor
+        FROM 
+            product
+        JOIN 
+            precio ON product.id_Precio = precio.id
+    """)
+    rows = cursor.fetchall()
+    
+    # Lista para almacenar productos con sus precios
+    productos_con_precios = []
+    for row in rows:
+        codigo_producto, marca, codigo, nombre, fecha, valor = row
+        precio = {
+            'Fecha': fecha.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+            'Valor': float(valor)
+        }
+        
+        codigo_producto, marca, codigo, nombre, fecha, valor = row
+        producto = {
+            'Codigo_del_producto': codigo_producto,
+            'Marca': marca,
+            'Codigo': codigo,
+            'Nombre': nombre,
+            'Precio': [precio]
+        }
+        productos_con_precios.append(producto)
+    
+    return render_template('hola.html', productos=productos_con_precios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
